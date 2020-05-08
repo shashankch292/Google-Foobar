3a53675af65dd618593e06335f08daa2cec10f53
shahinshelkar <60425843+shahinshelkar@users.noreply.github.com>
2020-05-08 20:12:50 +0530

    refactoring the code

--- Google Foobar/Solution_2B.java
+++ Google Foobar/Solution_2B.java
@@ -1,88 +1,58 @@
-package com.google.challenges; 
-import java.util.Arrays;
+package com.google.challenges;
+import java.util.*;
 
-public class Answer {   
-    public static int answer(int[] l) 
-    {
-        int b0[]=new int[10];
-        int b1[]=new int[10];
-        int b2[]=new int[10];
-        int l1[]=new int[10];
-        int b0n=0,b1n=0,b2n=0,l1n=0,sum=0;
-        int ib1=0,ib2=0;
-        for(int d : l)
-        {
-            sum+=d;
-            l1[l1n++]=d;
-            if(d%3==0)
-                b0[b0n++]=d;
-            else
-            if(d%3==1)
-                b1[b1n++]=d;
-            else
-            if(d%3==2)
-                b2[b2n++]=d;
-        }
-        
-        //System.out.println(sum);
-        
-        Arrays.sort(b0,0,b0n);
-        Arrays.sort(b1,0,b1n);
-        Arrays.sort(b2,0,b2n);
-        
-        if(sum%3==0)
-        {
-            int i,res=0;
-            Arrays.sort(l1,0,l1n);
-            for(i=l1n-1;i>=0;i--)
-            {
-                res=res*10+l1[i];
-            }
-            return res;
-        }
-        else
-        if(sum%3==1)
-        {            
-            if(b1n!=0)
-                ib1++;
-            else
-            if(b2n>=2)
-                ib2=ib2+2;
-            else
-                return 0;
-        }
-        else
-        if(sum%3==2)
-        {
-            if(b2n!=0)
-                ib2++;
-            else
-            if(b1n>=2)
-                ib1=ib1+2;
-            else
-                return 0;
-        }
-        
-        int fnl[]=new int[20];
-        int i,fn=0,res=0;
-        
-        for(i=0;i<b0n;i++)
-            fnl[fn++]=b0[i];
-        for(i=ib1;i<b1n;i++)
-            fnl[fn++]=b1[i];
-        for(i=ib2;i<b2n;i++)
-            fnl[fn++]=b2[i];
-        
-        Arrays.sort(fnl,0,fn);
-        
-        for(i=fn-1;i>=0;i--)
-        {
-            res=res*10+fnl[i];
-        }
-        
-        return res;
+public class Answer {
+  public static int answer(int[] nums) {
+    Arrays.sort(nums);
+    LinkedList<Integer> remainderAsOne = new LinkedList<>();
+    LinkedList<Integer> remainderAsTwo = new LinkedList<>();
+    LinkedList<Integer> remainderAsZero = new LinkedList<>();
+    int totalSum = 0;
+    for (int num : nums) {
+      if (num % 3 == 0)
+        remainderAsZero.add(num);
+      else if (num % 3 == 1)
+        remainderAsOne.add(num);
+      else if (num % 3 == 2)
+        remainderAsTwo.add(num);
+      totalSum += num;
+    }
+    if (totalSum % 3 == 0) {
+      return mergeAndGetNumber(remainderAsZero, remainderAsOne, remainderAsTwo);
+    } else if (totalSum % 3 == 1) {
+      if (remainderAsOne.size() > 0)
+        remainderAsOne.removeFirst();
+      else if (remainderAsTwo.size() > 1) {
+        remainderAsTwo.removeFirst();
+        remainderAsTwo.removeFirst();
+      } else
+        return 0;
+    } else if (totalSum % 3 == 2) {
+      if (remainderAsTwo.size() > 0)
+        remainderAsTwo.removeFirst();
+      else if (remainderAsOne.size() > 1) {
+        remainderAsOne.removeFirst();
+        remainderAsOne.removeFirst();
+      } else
+        return 0;
+    }
+    return mergeAndGetNumber(remainderAsZero, remainderAsOne, remainderAsTwo);
+  }
 
-        // Your code goes here.
+  private static int mergeAndGetNumber(LinkedList<Integer> remainderAsZero,
+                                       LinkedList<Integer> remainderAsOne,
+                                       LinkedList<Integer> remainderAsTwo) {
+    List<Integer> output = new ArrayList<>(remainderAsZero);
+    output.addAll(remainderAsOne);
+    output.addAll(remainderAsTwo);
+    Collections.sort(output);
+    return getNumber(output);
+  }
 
-    } 
+  private static int getNumber(List<Integer> output) {
+    int answer = 0;
+    for (int i = output.size() - 1; i >= 0; i--)
+      answer = answer * 10 + output.get(i);
+    return answer;
+  }
 }
